6045e8256ff2cb6cedbedc3d8e0b6272b53425e7
oebfx <99594672+oebfx@users.noreply.github.com>
2022-02-13 18:40:57 +0300

    без адаптива

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.main_slider', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.main_slider_arrow',
+
+    },
+
+});
